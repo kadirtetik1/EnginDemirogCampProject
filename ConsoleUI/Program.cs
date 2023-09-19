5618be1b1aa92902e7dd4aaad90e63af7bcf5440
kadirtetik1 <tetik.kadir@hotmail.com>
2023-09-19 17:18:59 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,11 @@
+﻿using Entities.Concrete;
+using System.Net.Http.Headers;
+
+Console.WriteLine("Hello, World!");
+
+
+Product product = new Product();
+
+product.ProductName = "Test";
+
+Console.WriteLine(product.ProductName);
