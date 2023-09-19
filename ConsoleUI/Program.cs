5846409a838dfa216c072ac1b7af5d12c282f7f1
kadirtetik1 <tetik.kadir@hotmail.com>
2023-09-19 18:11:51 +0300

    update

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -2,10 +2,3 @@
 using System.Net.Http.Headers;
 
 Console.WriteLine("Hello, World!");
-
-
-Product product = new Product();
-
-product.ProductName = "Test";
-
-Console.WriteLine(product.ProductName);
